428afb5c639ee145666cd39210828d287c8b25a7
Elisey <eliseyblajen@gmail.com>
2025-06-19 06:07:00 +0500

    Add services

--- migrations/env.py
+++ migrations/env.py
@@ -8,7 +8,7 @@ from alembic import context
 from sqlalchemy.ext.asyncio import create_async_engine
 
 from app.cmwp_bot.db.session import DATABASE_URL, Base
-from app.cmwp_bot.db import models 
+from app.cmwp_bot.db import models
 
 config = context.config
 fileConfig(config.config_file_name)
@@ -17,24 +17,38 @@ logger = logging.getLogger('alembic.env')
 target_metadata = Base.metadata
 
 
+def include_object(object, name, type_, reflected, compare_to):
+    if type_ == "table" and (
+        name.startswith("auth_") or
+        name.startswith("django_") or
+        name.startswith("core_")
+    ):
+        return False
+    return True
+
+
 def run_migrations_offline() -> None:
+    """Run migrations in 'offline' mode."""
     url = DATABASE_URL
     context.configure(
         url=url,
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
+        include_object=include_object,
     )
     with context.begin_transaction():
         context.run_migrations()
 
 
 def do_run_migrations(connection):
+    """Run migrations in 'online' mode with async engine."""
     context.configure(
         connection=connection,
         target_metadata=target_metadata,
         compare_type=True,
         compare_server_default=True,
+        include_object=include_object,
     )
     with context.begin_transaction():
         context.run_migrations()
@@ -47,7 +61,6 @@ async def run_migrations_online() -> None:
         await connection.run_sync(do_run_migrations)
 
     await connectable.dispose()
-
 
 if context.is_offline_mode():
     run_migrations_offline()
